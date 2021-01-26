b72ebeb9797a129216e83bdaabf6dbde0e32ab03
adam2021 <adam.patel101@outlook.com>
2021-01-26 10:08:40 +0000

    Apple Core Location API connected to Emergency services API (finally) - can update location in real time and data will update in real time.

--- SOS-App/Model/LocationViewModel.swift
+++ SOS-App/Model/LocationViewModel.swift
@@ -10,27 +10,79 @@ import Combine
 import CoreLocation
 
 class LocationViewModel: NSObject, ObservableObject{
-  
-  @Published var userLatitude: Double = 0
-  @Published var userLongitude: Double = 0
-  
-  private let locationManager = CLLocationManager()
-  
-  override init() {
-    super.init()
-    self.locationManager.delegate = self
-    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
-    self.locationManager.requestWhenInUseAuthorization()
-    self.locationManager.startUpdatingLocation()
-  }
+    //MARK: Properties
+    @Published var content = [MyResult]()
+    @Published var userLatitude: Double = 0
+    @Published var userLongitude: Double = 0
+    @Published var isoCountryCode = ""{
+        didSet{
+            //load the data once you have a countrycode
+            loadData()
+        }
+    }
+    
+    private let locationManager = CLLocationManager()
+    //MARK: init
+    override init() {
+        super.init()
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.requestLocation()
+    }
+    //MARK: Functions
+    func loadData() {
+        
+        let url = URL(string: "https://emergency-phone-numbers.herokuapp.com/country/\(isoCountryCode)")!
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error { print(error); return }
+            do {
+                let decodedResponse = try JSONDecoder().decode(MyResult.self, from: data!)
+                // we have good data – go back to the main thread
+                DispatchQueue.main.async {
+                    // update our UI
+                    self.content = [decodedResponse]
+                }
+                
+            } catch {
+                print(error)
+            }
+        }.resume()
+        
+    }
 }
 
 extension LocationViewModel: CLLocationManagerDelegate {
-  
-  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-    guard let location = locations.last else { return }
-    userLatitude = location.coordinate.latitude
-    userLongitude = location.coordinate.longitude
-    print(location)
-  }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        userLatitude = location.coordinate.latitude
+        userLongitude = location.coordinate.longitude
+        print(location)
+        
+        CLGeocoder().reverseGeocodeLocation(location) { (placemark, error) in
+            if error != nil
+            {
+                print("there is an error \(error)")
+            }
+            else
+            {
+                if let place = placemark?[0]
+                {
+                    self.isoCountryCode = place.isoCountryCode ?? ""
+                }
+            }
+        }
+        
+    }
+    //You have to respond to errors
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    //You have to respond to changes in autorization too
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        
+    }
 }
+
